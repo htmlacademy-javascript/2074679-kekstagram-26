1fd93f78d269c33a73aa556a63f40c47c1ae0225
Keks <keks@htmlacademy.ru>
2022-06-09 19:52:27 +0300

    :heavy_check_mark: Сборка #2

--- 2/js/main.js
+++ 2/js/main.js
@@ -1,27 +1,18 @@
 //Function to generate random integer inclusive
 //Random algorithm from there:
 //https://developer.mozilla.org/ru/docs/Web/JavaScript/Reference/Global_Objects/Math/random
-const getRandomNumber = (min, max) => {
+const getRandomNumberFromRange = (min, max) => {
   if(min < 0 || max < 0 || min === max) {
     throw new Error('Min/max values is incorrect.');
   }
   if(min > max) {
     [min, max] = [max, min]; // if min > max => mixing values
   }
-  min = Math.ceil(min);
-  max = Math.floor(max);
-  const randNumber = Math.floor(Math.random() * (max - min + 1)) + min;
-  return randNumber;
+  return Math.floor(Math.random() * (max - min + 1)) + min;
 };
 
 //Function to check text for max length
-const checkMaxLength = (text, maxLength) => {
-  if(text.length <= maxLength) {
-    return true;
-  } else {
-    return false;
-  }
-};
+const checkCommentMaxLength = (text, maxLength) => text.length <= maxLength;
 
-getRandomNumber(0,100);
-checkMaxLength('Hello', 4);
+getRandomNumberFromRange(0,100);
+checkCommentMaxLength('Hello', 4);
