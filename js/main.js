0dbf15ca7f4126106ba5b39763d1676d6d2b113f
Mikhail Likstanov <likstanov@gmail.com>
2022-07-20 18:23:12 +0300

    task 8.19 done

--- js/main.js
+++ js/main.js
@@ -1,6 +1,7 @@
 import {generateRandomPosts} from './generate-posts.js';
 import {renderPosts, bindPostClickListener} from './render-posts.js';
 import {renderModalPost} from './render-modal-post.js';
+import {createPost} from './create-post.js';
 
 const posts = generateRandomPosts(25);
 
@@ -9,3 +10,5 @@ renderPosts(posts);
 bindPostClickListener((postId) => {
   renderModalPost(posts.find((post) => post.id === +postId));
 });
+
+createPost();
